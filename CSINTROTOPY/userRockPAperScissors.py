7f78ed731673c2df4c433114872cf92b701d3a85
ktinson <monikai.tinson@verizon.com>
2024-10-14 16:02:44 -0500

    Recreated Rock paper scissors with user input

---
+++ CSINTROTOPY/userRockPAperScissors.py
@@ -0,0 +1,14 @@
+import random
+def main():
+   player1 = input("Choice: ").lower()
+   player2 = input("Choice: ").lower()
+   if player1 == player2:
+       print(f'player1: {player1} vs player2: {player2} = tie')
+   elif player2 == "rock" and player1 == "scissors" or player2 == "paper" and player1 == "rock" or player2 == "scissors" and player1 == "paper":
+       print(f'player1: {player1} vs player2: {player2} = player 2 wins')
+   else:
+        print(f'player1: {player1} vs player2: {player2} = player 1 wins')
+
+def randomChoice():
+    return random.choice(['rock', 'paper', 'scissors'])
+main()
