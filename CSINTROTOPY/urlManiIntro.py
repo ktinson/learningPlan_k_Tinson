7f07b399fa4a980513924fe75a0d661d8e80e16f
ktinson <monikai.tinson@verizon.com>
2024-10-15 14:51:51 -0500

    regex practice

---
+++ CSINTROTOPY/urlManiIntro.py
@@ -0,0 +1,8 @@
+import re
+url = input("URL: ").strip()
+
+if matches := re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE):
+    print(f"Username:", matches.group(1))
+
+# username = re.sub(r"^(https?://)?(www\.)?twitter\.com/", "", url)
+# print(f'Username: {username}')
