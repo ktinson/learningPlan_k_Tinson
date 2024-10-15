9bb307a6364db45aff65fb4836e2e66cfd533587
ktinson <monikai.tinson@verizon.com>
2024-10-15 15:53:41 -0500

    Pushing in preparation of adding properties

--- CSINTROTOPY/OOP/userOOPIntro.py
+++ CSINTROTOPY/OOP/userOOPIntro.py
@@ -1,11 +1,25 @@
 class Person:
-    def __init__(self, name, position):
+    def __init__(self, name, position, fitness):
         if not name:
             raise ValueError("Input missing")
         self.name = name
         self.position = position
+        self.fitness = fitness
     def __str__(self):
         return f"I'm a person and my name is {self.name} and my position is {self.position}"
+    def workout(self):
+        match self.fitness:
+            case "Lazy":
+                return "😴"
+            case "Sedenatary":
+                return "🗻"
+            case "Moderate":
+                return "🤸🏿‍♂️"
+            case "Extreme":
+                return "🐎"
+            case _:
+                return "🌪️"
+
 def main():
     name = get_name()
     position = get_position()
@@ -14,14 +28,16 @@ def main():
     personC = get_perClass()
     print(f'This is using a class: {personC.name} and they are {personC.position}')
     print(personC)
+    print(personC.workout())
     print(f'{name} and {position}') 
     print(f'{person[0]} and {person[1]}')
     print(f"{personD['nameD']} and {personD['positionD']}")
 def get_perClass():
     name = input("Name:")
     position = input("Position: ")
+    fitness = input("Fitness level: ")
     try:
-        return Person(name, position)
+        return Person(name, position, fitness)
     except ValueError:
         get_perClass()
 
