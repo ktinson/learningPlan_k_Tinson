1bbec6097ae1fee654ab95d8ab2810f16304111d
ktinson <monikai.tinson@verizon.com>
2024-10-15 14:27:53 -0500

    Validation

--- CSINTROTOPY/validateIntro.py
+++ CSINTROTOPY/validateIntro.py
@@ -1,5 +1,6 @@
 import re
 email = input("Enter Email: ").strip()
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 try:
     #. 0 or more characters  before the @
     #any character except an at [^@] so @ can only appear once
@@ -7,7 +8,8 @@ try:
     #if re.search(r"^[^@]+@[^@]+\.edu$", email):
     #if re.search(r"^[a-zA-Z09_]-+@[a-zA-Z09_]+\.edu$", email):
     #\w any word char including numbers and _
-    if re.search(r"^\w+@\w+\.(edu$|com$)", email, re.IGNORECASE):
+    #if re.search(r"^\w+@(\w\.)?\w+\.(edu$|com$)", email, re.IGNORECASE):
+    if re.search(regex, email, re.IGNORECASE):
         print("Valid")
     else:
         print("Invalid")
