fd842945b32b1cff87a58868e515cc7f9934c850
ktinson <monikai.tinson@verizon.com>
2024-10-15 08:55:18 -0500

    Added try catch, sys, num

--- CSINTROTOPY/commandLineArgsIntro.py
+++ CSINTROTOPY/commandLineArgsIntro.py
@@ -3,11 +3,18 @@ import sys
 try:
     if len(sys.argv) < 2:
      print("not enough args")
+     sys.exit("Exiting with sys.exit...")
     elif len(sys.argv) > 2:
      print("too many args")
+     sys.exit("Exiting with sys.exit...")
+
     else:
         print("Hello, my name is: ", sys.argv[1])
         print("This is the name of the program:", sys.argv[0])
+        sys.exit("Exiting with sys.exit...")
+
 except IndexError:
     print("Need argument")
+    sys.exit("Exiting with sys.exit...")
+
 
