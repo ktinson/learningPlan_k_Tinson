f1ef2e07926e54433d0bc2dc4ce11623e33d5f04
ktinson <monikai.tinson@verizon.com>
2024-10-16 14:01:59 -0500

    Started weater app

--- CSINTROTOPY/OOP/classMethodIntro.py
+++ CSINTROTOPY/OOP/classMethodIntro.py
@@ -1,8 +1,12 @@
 import random
 class Personality:
-    def __init__(self):
-        self.personalities = ["INTJ"," ESTJ", "ENTJ", "ESFJ", "ENFJ", "ISTJ", "ISFJ", "INFJ", "ESTP", "ESFP", "ENTP", "ENFP", "ISTP", "ISFP", "INTP", "INFP"]
-    def sort(self, name):
-        print(name, "has the personality type", random.choice(self.personalities))
-personality = Personality()
-personality.sort("Bob")
+    # def __init__(self):
+     #self.personalities = [works] when not using class method
+    personalities = ["INTJ"," ESTJ", "ENTJ", "ESFJ", "ENFJ", "ISTJ", "ISFJ", "INFJ", "ESTP", "ESFP", "ENTP", "ENFP", "ISTP", "ISFP", "INTP", "INFP"]
+    @classmethod
+    def sort(cls, name):#cls is class, we are using the varibale to refer to the class in the function
+        #def sort(cls,name)
+        print(name, "has the personality type", random.choice(cls.personalities))
+        #random.choice(self.personalities)
+#personality = Personality()
+Personality.sort("Bob")
