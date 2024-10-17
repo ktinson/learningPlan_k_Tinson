16a88c8055d73eacdf9ba5ee8ac08d12ff72a92e
ktinson <monikai.tinson@verizon.com>
2024-10-17 16:33:34 -0500

    Commiting in preparatiion for AWS

--- PythonJS/weatherTracker/app/signals.py
+++ PythonJS/weatherTracker/app/signals.py
@@ -6,7 +6,7 @@ from django.dispatch import receiver
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    if not created:
+    if created:
         Profile.objects.create(user=instance)
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
--- PythonJS/weatherTracker/frontend/src/components/App.js
+++ PythonJS/weatherTracker/frontend/src/components/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import ReactDOM from 'react-dom'; 
+import ReactDOM from 'react-dom/client'; 
 export default class App extends Component{
     constructor(props){ 
         super(props)
