1388c4ca8941c0cd4d47aa48ed17e778d4dc1ee7
ktinson <monikai.tinson@verizon.com>
2024-10-22 09:09:25 -0500

    Checking database

--- PythonWD/frontend/src/api.js
+++ PythonWD/frontend/src/api.js
@@ -3,7 +3,7 @@ let apiURL;
 if (process.env.NODE_ENV === "development") {
   apiURL = process.env.REACT_APP_API_URL || "http://localhost:8000";
 } else {
-  apiURL = "https://learningplan-k-tinson-main.onrender.com" || "https://learningplan-k-tinson.onrender-frontend.com" || "https://learningplan-k-tinson.onrender.com";
+  apiURL = "https://learningplan-k-tinson.onrender.com";
 }
 // || "https://learningplan-k-tinson.onrender.com";
 export default apiURL;
