ef28657a53689f4e21194c1f0240751a3f08143a
ktinson <monikai.tinson@verizon.com>
2024-10-22 14:20:16 -0500

    Added url if, but need to add set node -

--- PythonWD/frontend/src/api.js
+++ PythonWD/frontend/src/api.js
@@ -1,9 +1,11 @@
 let apiURL;
 
 if (process.env.NODE_ENV === "development") {
-  apiURL = process.env.REACT_APP_API_URL || "https://learningplan-k-tinson.onrender.com"
-} else {
+  apiURL = process.env.REACT_APP_API_URL|| "http://localhost:8000" 
+} else if(process.env.NODE_ENV === "production") {
   apiURL = "https://learningplan-k-tinson.onrender.com";
+}else{
+  console.log("erorororo ")
 }
 // || "https://learningplan-k-tinson.onrender.com";
 export default apiURL;
